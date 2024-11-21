ecc41f75f055dd8e0f0d04a93984e95bf73f1eee
iDIOtu <95068240+iDIOtu@users.noreply.github.com>
2024-11-21 19:55:42 +0500

    toska3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,11 +1,11 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.*;
 
 public class Task03Main {
 
@@ -19,6 +19,22 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Map<String, Set<String>> anagrams = new TreeMap<>();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, charset))) {
+            br.lines()
+                    .map(String::toLowerCase)
+                    .filter(x -> x.matches("[а-я]+") && x.length() >= 3)
+                    .collect(Collectors.toList())
+                    .forEach(c -> {
+                        char[] chs = c.toCharArray();
+                        Arrays.sort(chs);
+                        String el = new String(chs);
+                        anagrams.computeIfAbsent(el, x -> new TreeSet<>()).add(c); });
+        } catch (IOException ignored) {}
+
+        return anagrams.values().stream()
+                .filter(x -> x.size() >= 2)
+                .collect(Collectors.toList());
     }
 }
